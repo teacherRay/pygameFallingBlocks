04ccc813738d2c014aaabb64ca1d19f62eb28576
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-11 12:54:45 +0700

    4. Moving cube

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ RED =(255,0,0)
 
 player_pos = [400,300]
 player_size = 50
-    
+   
 SCREENWIDTH = 800
 SCREENHEIGHT = 600
 screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
@@ -20,6 +20,19 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+
+                x = player_pos[0]
+                y = player_pos[1]
+
+                if event.key == pygame.K_LEFT:
+                    x -= 5
+                elif event.key == pygame.K_RIGHT:
+                    x += 5
+                player_pos = [x,y]
+
+    screen.fill((0,0,0))        
 
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
 
