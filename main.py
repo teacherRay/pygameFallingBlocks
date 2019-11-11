6a68ebe13bdc2166a12966a6a4610f665ffd3085
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-11 14:18:09 +0700

    5. falling blocks

--- main.py
+++ main.py
@@ -2,18 +2,34 @@
 #git remote add origin https://github.com/teacherRay/pygameFallingBlocks.git
 import sys
 import pygame
+import random
+
 pygame.init()
 
-RED =(255,0,0)
-
-player_pos = [400,300]
-player_size = 50
-   
 SCREENWIDTH = 800
 SCREENHEIGHT = 600
+RED = (255,0,0)
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+
+enemy_x = random.randint(50,SCREENWIDTH)
+enemy_y = random.randint(50,300)
+
+player_size = 50
+player_pos = [SCREENWIDTH / 2, SCREENHEIGHT - 2 * player_size]
+enemy_size = 50
+enemy_pos = [enemy_x, enemy_y]
+   
 screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
 
 game_over = False
+
+clock = pygame.time.Clock()
+
+def spawn_enemy():
+    enemy_x = random.randint(50,SCREENWIDTH)
+    enemy_y = random.randint(50,300)
+
 
 while not game_over:
 
@@ -23,18 +39,26 @@ while not game_over:
 
         if event.type == pygame.KEYDOWN:
 
-                x = player_pos[0]
-                y = player_pos[1]
-
                 if event.key == pygame.K_LEFT:
-                    x -= 5
+                    player_pos[0] -= 40
                 elif event.key == pygame.K_RIGHT:
-                    x += 5
-                player_pos = [x,y]
+                    player_pos[0] += 40
 
-    screen.fill((0,0,0))        
+    screen.fill((BLACK))  
+  
 
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+    
+    if enemy_y >= 0 and enemy_y <= SCREENHEIGHT:
+        enemy_y += 5
+
+    if enemy_y >= SCREENHEIGHT:
+       enemy_y = random.randint(50,300)
+       enemy_x = random.randint(50,SCREENWIDTH-50)  
+
+    pygame.draw.rect(screen, BLUE, (enemy_x, enemy_y, player_size, player_size))
+
+    clock.tick(30)
 
     pygame.display.update()
 
