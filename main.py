910d8713f16409c7087dd0d2d14715d4b7cc71af
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-11 12:07:00 +0700

    2: setup screen and game loop

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #https://www.youtube.com/watch?v=-8n91btt5d8
 #git remote add origin https://github.com/teacherRay/pygameFallingBlocks.git
+import sys
 import pygame
+pygame.init()
+SCREENWIDTH = 800
+SCREENHEIGHT = 600
+screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+
+game_over = False
+
+while not game_over:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
